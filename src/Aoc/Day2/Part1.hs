8ec22ccdab6f3afc2ac468acb32cc6a250ec5ec9
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-11-28 19:21:43 +0100

    Day 2

--- src/Aoc/Day2/Part1.hs
+++ src/Aoc/Day2/Part1.hs
@@ -1,6 +1,29 @@
 module Aoc.Day2.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Map as Map
+import Data.Maybe
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve (x:xs) = show $ fromJust . Map.lookup 0 . iterateIndex 0 $ getAdjustedOpcodeMap x
+
+getAdjustedOpcodeMap :: BS.ByteString -> Map.Map Int Int
+getAdjustedOpcodeMap = Map.insert 1 12 . Map.insert 2 2 . getOpcodeMap
+
+getOpcodeMap :: BS.ByteString -> Map.Map Int Int
+getOpcodeMap bs = Map.fromList $ zip [0..] opcodeList
+  where
+    opcodeList :: [Int]
+    opcodeList = fmap (fst . fromJust . BC.readInt) $ BC.split ',' bs
+
+iterateIndex :: Int -> Map.Map Int Int -> Map.Map Int Int
+iterateIndex i m = case Map.lookup i m of
+    Nothing -> m
+    Just 99 -> m
+    Just 2 -> iterateIndex (i + 4) $ applyOpcode (*) m
+    Just 1 -> iterateIndex (i + 4) $ applyOpcode (+) m
+  where
+    lookupOffset k = m Map.! (i + k)
+    lookup2Offset k = m Map.! (lookupOffset k)
+    applyOpcode fn = Map.insert (lookupOffset 3) (fn (lookup2Offset 1) (lookup2Offset 2))
--- src/Aoc/Day2/Part2.hs
+++ src/Aoc/Day2/Part2.hs
@@ -1,7 +1,21 @@
 module Aoc.Day2.Part2 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
 import Aoc.Day2.Part1
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve (x:xs) = show $ 100 * (fst findSuccess) + snd findSuccess
+  where
+    findSuccess = fromJust $ find predicate getAllAttempts
+    predicate = (==) 19690720 . getResult
+    getResult (noun, verb) = fromJust . Map.lookup 0 . iterateIndex 0 $ getAdjustedOpcodeMap' noun verb x
+
+getAdjustedOpcodeMap' :: Int -> Int -> BS.ByteString -> Map.Map Int Int
+getAdjustedOpcodeMap' noun verb = Map.insert 1 noun . Map.insert 2 verb . getOpcodeMap
+
+getAllAttempts :: [(Int, Int)]
+getAllAttempts = [(x, y) | x <- [0..99], y <- [0..99]]
